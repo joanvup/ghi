fc06ead36733e0d31657318761b108d025d950b0
Joan Fuentes <joanvup@gmail.com>
2026-04-10 09:10:26 -0500

    feat: initialize axios instance with request and response interceptors for API authentication

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 // Por ejemplo: 'http://localhost/tu-carpeta/backend'
 // const API_URL = 'http://localhost/api'; 
 // const API_URL = 'http://ghi.test/backend';
-const API_URL = 'http://ghi.test/ghi/backend';
+// const API_URL = 'http://ghi.test/ghi/backend';
+const API_URL = 'https://ghi.colegiobilingue.edu.co/api';
 
 const api = axios.create({
     baseURL: API_URL,
